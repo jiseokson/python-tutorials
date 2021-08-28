a7386ccb82fd407df2a826575f8a4cdfc7484e14
Jiseok Son <jiseokson@gmail.com>
2021-08-28 20:05:34 +0900

    word cnt 예제 content 수정

--- word-cnt.py
+++ word-cnt.py
@@ -1,12 +1,9 @@
 content = '''
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti assumenda illum magni sint veniam iste corporis in molestiae sed expedita id dicta modi, reprehenderit quo!
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis aspernatur ipsa voluptatibus temporibus dolores voluptatem id minus at quisquam eos.
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero beatae deserunt, possimus modi sint voluptatibus odio cupiditate eaque sequi iste ea eligendi labore aut maiores, inventore suscipit. Earum, fugiat eligendi.
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit numquam enim voluptatum optio, deleniti iste quasi cupiditate.
-
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos laboriosam odit nam labore illo?
-Lorem ipsum dolor sit amet.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam architecto, deserunt fuga exercitationem optio eaque fugit.
+The US Embassy in Kabul on Friday again warned US citizens at a number of gates at Hamid Karzai International Airport to "leave immediately," citing security threats.
+The alert advised US citizens "to avoid traveling to the airport and to avoid airport gates."
+Flights could be seen taking off Saturday but it was unclear how many people were being allowed into the airport.
+An eyewitness told CNN he saw Taliban members fire shots in the air outside the main Kabul airport entrance gate on Saturday morning to disperse crowds that had gathered again in attempts to flee Afghanistan.
+Far fewer people were at the airport Friday, and a source directly familiar with the Kabul airport operations told CNN the remaining focus was to get people with special, last-minute access requests to the airport. But the source cautioned that officials were "unsure how many they can get in with so much Taliban coordination required."
 '''
 
 words = {}
@@ -14,6 +11,7 @@ for line in content.split('\n'):
     line = line.strip()
     for word in line.split(' '):
         word = word.strip()
+        word = word.lower()
         if word == '':
             continue
         words[word] = words.get(word, 0) + 1
